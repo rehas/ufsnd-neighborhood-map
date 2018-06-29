3345aef1e1370675350149e48e07e7ee4a911cb8
RehaS <beratson@gmail.com>
2018-06-29 14:21:03 +0300

    Simple KO functionaluty extended with catching events

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,17 @@
 function nMapViewModel(){
-  var self = this;
+  let self = this;
   self.test1 = "tester";
+
+  self.array1 = ko.observableArray();
+  self.array1.push({name: "eleman1"});
+  self.array1.push({name: "eleman2"});
+
+  self.changeColor = function(element, event){
+    //element.
+    console.log(element);
+    console.log("element");
+    $(event.target).css('color', "red");
+  };
 }
 ko.applyBindings(new nMapViewModel());
 
