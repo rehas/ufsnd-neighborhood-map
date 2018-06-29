8e4f3a3e105b3b3fe89ae441ced48af8d388ce08
RehaS <beratson@gmail.com>
2018-06-29 18:40:46 +0300

    Knockout list implemented
    List item hover and click events captured
    Search input UI complete, list filtering to be implemented

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 function nMapViewModel(){
   let self = this;
   self.test1 = "tester";
+  self.query = ko.observable('');
 
   self.array1 = ko.observableArray();
   self.array1.push({name: "eleman1"});
@@ -9,9 +10,20 @@ function nMapViewModel(){
   self.changeColor = function(element, event){
     //element.
     console.log(element);
-    console.log("element");
+    console.log("hover");
     $(event.target).css('color', "red");
   };
+  self.clicked = function(element, event){
+    //element.
+    console.log(element);
+    console.log("clicked");
+    $(event.target).css('color', "yellow");
+  };
+  self.search = function(){
+    console.log("Searched");
+  }
+
+  self.query.subscribe(self.search);
 }
 ko.applyBindings(new nMapViewModel());
 
